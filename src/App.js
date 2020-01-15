214263d63836fdba0f33502a3f4e78ca5f0eb205
Rahul Shah <rahulshah7@outlook.com>
2020-01-15 05:09:33 +0000

    Add authentication
    - Sign in
    - Sign out
    - Sign up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import authService from "./services/auth";
+import entriesService from "./services/entries";
+import SignUpForm from "./components/SignUpForm";
+import SignInForm from "./components/SignInForm";
+import Entry from "./components/Entry";
 
 function App() {
+  const [user, setUser] = useState({});
+  const [entries, setEntries] = useState([]);
+  useEffect(() => {
+    entriesService.getAll().then(res => setEntries(res.data));
+  }, []);
+
+  const handleSignInRequest = async signInCredentials => {
+    const res = await authService.signIn(signInCredentials);
+    setUser(res.data);
+    // redirect to "/" route
+  };
+
+  const handleSignUpRequest = async signUpCredentials => {
+    const res = await authService.signUp(signUpCredentials);
+    setUser(res.data);
+    // redirect to "/" route
+  };
+
+  const handleSignOutRequest = async () => {
+    const res = await authService.signOut();
+    setUser({});
+    // redirect to "/" route
+  };
+
   return (
     <div>
       <h1>retroFlect</h1>
+      <h2>{user.firstName} is signed in.</h2>
+      <button onClick={handleSignOutRequest}>Sign Out</button>
+      <SignInForm handleRequest={handleSignInRequest} />
+      <br></br>
+      <br></br>
+      <SignUpForm handleRequest={handleSignUpRequest} />
+      <br></br>
+      <br></br>
+      <Entry entries={entries}></Entry>
     </div>
   );
 }
---
+++ src/components/Entry.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Entry = ({ entries }) => {
+  return (
+    <>
+      <h3>Entry</h3>
+      <h4>Questions</h4>
+      <h4>Moods</h4>
+      <h4>Diary</h4>
+    </>
+  );
+};
+
+export default Entry;
