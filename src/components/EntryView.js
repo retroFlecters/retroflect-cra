f5fd55edd53cc7d6abb778460a59e0d401e6ea99
djlonboy <djlonboy@gmail.com>
2020-01-15 14:13:51 +0000

    Displays single entry with date  - uses textareas for fields  - Maps through entries JSON and extracts diary and date  - Formats date in standard day/month/year format

--- src/components/EntryView.js
+++ src/components/EntryView.js
@@ -1,11 +1,27 @@
 import React from "react";
 
 const EntryView = ({ entries }) => {
+
+  const entryDiary = entries.map(entry => entry.diary)
+  const entryDate = entries.map(entry => entry.entryDate)
+
   return (
     <div>
-      {JSON.stringify(entries)}
+    Entry Date:
+    <br />
+      <textarea rows={1} cols={50} value={FormatDate(entryDate[0])} />
+      <br />
+      Diary Entry:
+      <br />
+      <textarea rows={10} cols={100} value={entryDiary[0]} />
     </div>
   );
+
+};
+
+const FormatDate = (date) => {
+  let postDate = new Date(date);
+  return postDate.toLocaleDateString()
 };
 
 export default EntryView;
